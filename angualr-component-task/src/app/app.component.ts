d8962b0f46aa3c8816306534af6e6aae1c7328f4
RahafJazar <rahafjazar73@gmail.com>
2025-02-26 17:55:54 +0300

    change on design

--- angualr-component-task/src/app/app.component.ts
+++ angualr-component-task/src/app/app.component.ts
@@ -2,13 +2,28 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { animate, group, query, style, transition, trigger } from '@angular/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+
+
+  animations: [
+    trigger('routeAnimations', [
+      transition('HomePage => LoginPage', [
+        query(':enter', [
+          style({ transform: 'translateY(-100%)', opacity: 0 }), // Start fully above
+          animate('1s ease-out',
+            style({ transform: 'translateY(0)', opacity: 1 })) // Smoothly slide down
+        ], { optional: true })
+      ])
+    ])
+  ]
 })
 export class AppComponent implements OnInit {
   ngOnInit(): void {
@@ -16,5 +31,15 @@ export class AppComponent implements OnInit {
     // window.location.assign("/home")
   }
   title = 'angualr-component-task';
-
+  prepareRoute(outlet: RouterOutlet) {
+    return outlet?.activatedRouteData?.['animation'];
+  }
 }
+
+
+
+
+
+
+
+
