9c3c3f65d4a259fb4db0d0608ee459c8be183b24
RahafJazar <rahafjazar73@gmail.com>
2025-03-03 16:09:44 +0300

    angular-module

---
+++ angular-module/src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { AboutComponent } from './app/about/about.component';
+
+export const routes: Routes = [
+
+    {
+        path: 'about',
+        component: AboutComponent
+    }
+];
