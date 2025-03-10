b72778a199a60c87c74f8510ca48ca0bcda4fbc5
RahafJazar <rahafjazar73@gmail.com>
2025-03-10 14:53:37 +0300

    simple preactice website

---
+++ angular-simple-site-main/angular-simple-site-main/README.md
@@ -0,0 +1,33 @@
+# angular-simple-site
+
+Doc and live codding : [https://www.hockeycomputindo.com/2022/07/angular-simple-site-skeleton-starter.html](https://www.hockeycomputindo.com/2022/07/angular-simple-site-skeleton-starter.html)
+
+Demo : [https://mesinkasirrestoran.vercel.app/](https://mesinkasirrestoran.vercel.app/)
+
+Demo minimalist : [https://mesinkasir.vercel.app/](https://mesinkasir.vercel.app/)
+
+Fork Stackblitz Source Code Now : [https://www.hockeycomputindo.com/2022/07/angular-simple-site-skeleton-starter.html](https://www.hockeycomputindo.com/2022/07/angular-simple-site-skeleton-starter.html)
+
+<a href="https://www.buymeacoffee.com/axcora"><img width="240" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgIA9HMwkK8kr7uRwVNxnhXsLQsJHxQQYVSzqCAaK58OpJOiTlzbIX7eEwS_VpJ3oEG-xrmVEl2WKqGvB_o-KjyBGTbbjFHM_bN2Jce9g3FTnt2ZJViwcvB9DHPOKPEMCl7jTQRVWKPw_ETloH7_CK8Xr09SSNNx22xnfGjViwdEsGtR-yGrLmr-JUGHA/s1090/bmc-button.png"/></a>
+
+---
+
+![angular website template](https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhwXzuvYpHExjYRYuAnZ32r7JIz3Ixszt3lTDVHmFhFYnF2zB5zfYdfMpBA3vnvN_4yaYEH4goAIzM02K65RPr_O3opnQWI_0rDheIB9ChtkCvjEFGJWyYRBZTS-49wD8yJlLr4X2ZwnFsJml_vHJ19vGJ_TjHe7koeqUMiTEnCm3Xu1UxYwbxG72WIaA/s988/sveltekit%20web%20template%20seo.png)
+
+simple angular skeleton display
+
+![angular website template](https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgehFz4IF4rNTzstfRCkDsq8xfyBXWE9nrmgnJi_EIzBfxMu0ZhQQLWUbhADUrEAMconswpS-XL98XqVnP9odFjz1WqJ9V0cD7PyrKlmj0bXw2yTHvaVdodnVuo8N3mwMBWfTo3L4_i-MtIBbKGq1t_CJlAVETt_cHiqBW3ILGvPReGTpZEr4u22kD-pA/s1349/angular%20seo.png)
+
+lighthouse test
+
+---
+
+Doc and live codding : [https://www.hockeycomputindo.com/2022/07/angular-simple-site-skeleton-starter.html](https://www.hockeycomputindo.com/2022/07/angular-simple-site-skeleton-starter.html)
+
+Demo : [https://mesinkasirrestoran.vercel.app/](https://mesinkasirrestoran.vercel.app/)
+
+Demo minimalist : [https://mesinkasir.vercel.app/](https://mesinkasir.vercel.app/)
+
+Fork Stackblitz Source Code Now : [https://www.hockeycomputindo.com/2022/07/angular-simple-site-skeleton-starter.html](https://www.hockeycomputindo.com/2022/07/angular-simple-site-skeleton-starter.html)
+
+<a href="https://www.buymeacoffee.com/axcora"><img width="240" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgIA9HMwkK8kr7uRwVNxnhXsLQsJHxQQYVSzqCAaK58OpJOiTlzbIX7eEwS_VpJ3oEG-xrmVEl2WKqGvB_o-KjyBGTbbjFHM_bN2Jce9g3FTnt2ZJViwcvB9DHPOKPEMCl7jTQRVWKPw_ETloH7_CK8Xr09SSNNx22xnfGjViwdEsGtR-yGrLmr-JUGHA/s1090/bmc-button.png"/></a>
---
+++ angular-simple-site-main/angular-simple-site-main/src/app/component/home.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: '../design/home.html',
+  styleUrls: ['../app.component.css'],
+})
+export class HomeComponent implements OnInit {
+  link = '/';
+  judul = 'Mesin Kasir Restoran';
+  deskripsi = 'Lorep ipsum dolor siamet';
+  text1 =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum';
+  text2 =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum';
+  text3 =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum';
+  text4 =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum';
+  title1 = 'LOREP IPSUM DOLOR SIAMET';
+  text5 =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum';
+  title2 = 'LOREP IPSUM DOLOR SIAMET';
+  text6 =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum';
+  title3 = 'LOREP IPSUM DOLOR SIAMET';
+  text7 =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum';
+  title4 = 'LOREP IPSUM DOLOR SIAMET';
+  text8 =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum';
+  text8a =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum';
+  title5 = 'LOREP IPSUM DOLOR SIAMET';
+  text9 =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum';
+
+  constructor(private meta: Meta, private title: Title) {
+    this.meta.addTags([
+      {
+        name: 'description',
+        content:
+          'Pengusaha restoran cafe rumah makan dan kuliner wajib tau mesin kasir restoran ini ,waiters,kitchen ,kasir dan admin lengkap',
+      },
+    ]);
+    this.setTitle('Mesin Kasir Restoran');
+  }
+  public setTitle(newTitle: string) {
+    this.title.setTitle(newTitle);
+  }
+  ngOnInit() {}
+}
